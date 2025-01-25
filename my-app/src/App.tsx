542770ccbb0ad7728e6a9588e40bf3188b039158
Atulya Kadur <86684438+AtulyaK@users.noreply.github.com>
2025-01-25 15:07:47 -0500

    Inserting Locofy Design into React
    I made the react app as well as insert the pages into Their respective files into the React app. I tried doing it where it displays properly. However, it is not working right now. I will try to convert it into the App.tsx

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './global.css';
+import Main from './components/Main'
+function App() {
+  return (
+    <>
+      <div>
+        <Main />
+      </div>
+    </>
+  )
+}
+
+export default App
